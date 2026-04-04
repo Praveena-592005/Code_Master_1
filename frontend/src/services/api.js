712b73bb03309fc56c222378530a00bdb9f13bff
Praveena T <23csec18praveenathirumalai@gmail.com>
2026-04-04 21:04:32 +0530

    Final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000'
+    // Use your live Render URL here
+    baseURL: 'https://code-master-3.onrender.com'
 });
 
-export const fetchProblems = () => API.get('/');
-// You can add more API calls here as we build the backend routes
+// Fetch all problems
+export const fetchProblems = () => API.get('/api/problems');
+
+// Execute code (Run/Submit)
+export const executeCode = (data) => API.post('/api/execute', data);
+
+// Auth: Register
+export const registerUser = (userData) => API.post('/api/auth/register', userData);
+
+// Auth: Login
+export const loginUser = (credentials) => API.post('/api/auth/login', credentials);
+
+export default API;
