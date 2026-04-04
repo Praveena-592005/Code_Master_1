416dd1d9a2a5a5a5355fa820843d341a33a7d247
Praveena T <23csec18praveenathirumalai@gmail.com>
2026-04-04 17:40:15 +0530

    Commit final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://code-master-1-8eak.onrender.com'
+    // Adding /api here saves you from typing it every time below
+    baseURL: 'https://onrender.com' 
 });
 
-export const fetchProblems = () => API.get('/');
-// You can add more API calls here as we build the backend routes
+// Now this will call: https://onrender.com/problems
+export const fetchProblems = () => API.get('/problems');
+
+// You can now easily add your other routes:
+export const loginUser = (data) => API.post('/auth/login', data);
+export const registerUser = (data) => API.post('/auth/register', data);
